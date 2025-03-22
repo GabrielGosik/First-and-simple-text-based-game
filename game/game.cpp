c0d6640c3f3345cee60c70b3d5e19d811336953d
Gabriel Gosik <gabriel.gosik125@gmail.com>
2025-03-22 20:17:27 +0100

    Dodaj pliki projektów.

---
+++ game/game.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+//#include <unistd.h>
+#include <dos.h>
+#include <windows.h>
+
+
+using namespace std;
+int main()
+{
+    string charName;
+    char nameChoice;
+    int nameChoiceInt = 0;
+
+    cout << "Name your character: ";
+    cin >> charName;
+
+    do {
+        cout << endl << endl << "Your name shall be " << charName << endl << "Are you sure? (y/n): ";
+        cin >> nameChoice;
+        if (nameChoice == 'y') {
+            nameChoiceInt = 1;
+            cout << "The name was chosen. You're called " << charName;
+            Sleep(2000);
+            system("CLS");
+        }
+        else if (nameChoice != 'y' && nameChoice != 'n') cout << "Incorrect character used" << endl;
+
+    } while (nameChoiceInt > 1);
+    return 0;
+    
+}
+
